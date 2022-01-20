abdb6f7b343b92bf7c6c6e9d0ffe4bf53372d774
AeneasTews <Aeneas.tews@gmail.com>
2022-01-20 21:48:24 +0100

    Initial Commit

---
+++ queue-management/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  queue-management
+//
+//  Created by Aeneas Tews on 20.01.22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
